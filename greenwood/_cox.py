6e78abd6cd8963ccd6742bc217fd85ba2969c48c
Richard Iannone <riannone@me.com>
2026-07-03 16:34:27 -0400

    Revise logic in _cox_terms()

--- greenwood/_cox.py
+++ greenwood/_cox.py
@@ -132,41 +132,50 @@ def _cox_terms(
     grad = np.zeros(p)
     info = np.zeros((p, p))
 
-    for t in event_times:
-        at_risk = (entry < t) & (exit_ >= t)
-        dying = (exit_ == t) & event
+    for members, event_times in strata_groups:
+        xs = x[members]
+        es = entry[members]
+        xx = exit_[members]
+        ev = event[members]
+        ws = weight[members]
+        eta = xs @ beta
+        risk_score = np.exp(eta) * ws
 
-        rx = x[at_risk]
-        rr = risk_score[at_risk]
-        s0 = rr.sum()
-        s1 = rx.T @ rr
-        s2 = (rx * rr[:, None]).T @ rx
+        for t in event_times:
+            at_risk = (es < t) & (xx >= t)
+            dying = (xx == t) & ev
 
-        w_d = weight[dying]
-        loglik += float((w_d * eta[dying]).sum())
-        grad += (x[dying] * w_d[:, None]).sum(axis=0)
+            rx = xs[at_risk]
+            rr = risk_score[at_risk]
+            s0 = rr.sum()
+            s1 = rx.T @ rr
+            s2 = (rx * rr[:, None]).T @ rx
 
-        if ties == "breslow":
-            d_weight = float(w_d.sum())
-            z1 = s1 / s0
-            loglik -= d_weight * np.log(s0)
-            grad -= d_weight * z1
-            info += d_weight * (s2 / s0 - np.outer(z1, z1))
-        else:  # efron
-            dx = x[dying]
-            dr = risk_score[dying]
-            d0 = dr.sum()
-            d1 = dx.T @ dr
-            d2 = (dx * dr[:, None]).T @ dx
-            m = int(dying.sum())
-            for tie in range(m):
-                f = tie / m
-                denom = s0 - f * d0
-                z1 = (s1 - f * d1) / denom
-                z2 = (s2 - f * d2) / denom
-                loglik -= float(np.log(denom))
-                grad -= z1
-                info += z2 - np.outer(z1, z1)
+            w_d = ws[dying]
+            loglik += float((w_d * eta[dying]).sum())
+            grad += (xs[dying] * w_d[:, None]).sum(axis=0)
+
+            if ties == "breslow":
+                d_weight = float(w_d.sum())
+                z1 = s1 / s0
+                loglik -= d_weight * np.log(s0)
+                grad -= d_weight * z1
+                info += d_weight * (s2 / s0 - np.outer(z1, z1))
+            else:  # efron
+                dx = xs[dying]
+                dr = risk_score[dying]
+                d0 = dr.sum()
+                d1 = dx.T @ dr
+                d2 = (dx * dr[:, None]).T @ dx
+                m = int(dying.sum())
+                for tie in range(m):
+                    f = tie / m
+                    denom = s0 - f * d0
+                    z1 = (s1 - f * d1) / denom
+                    z2 = (s2 - f * d2) / denom
+                    loglik -= float(np.log(denom))
+                    grad -= z1
+                    info += z2 - np.outer(z1, z1)
 
     return loglik, grad, info
 
